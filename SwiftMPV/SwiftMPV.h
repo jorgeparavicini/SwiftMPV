4951d00de94e4cb73ee3c9dd1a1598b0563f73d8
Jorge Paravicini <j.m.paravicini@gmail.com>
2019-07-05 16:55:08 +0200

    Initial Commit

---
+++ SwiftMPV/SwiftMPV.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMPV.h
+//  SwiftMPV
+//
+//  Created by Jorge Paravicini on 05.07.19.
+//  Copyright © 2019 Jorge Paravicini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftMPV.
+FOUNDATION_EXPORT double SwiftMPVVersionNumber;
+
+//! Project version string for SwiftMPV.
+FOUNDATION_EXPORT const unsigned char SwiftMPVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMPV/PublicHeader.h>
+
+
