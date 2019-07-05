bde1619f822f21d643f62019dcacaffa3f3ce9f0
Jorge Paravicini <j.m.paravicini@gmail.com>
2019-07-05 17:01:13 +0200

    initial commit

---
+++ SwiftMPV/SwiftMPV.swift
@@ -0,0 +1,10 @@
+//
+//  SwiftMPV.swift
+//  SwiftMPV
+//
+//  Created by Jorge Paravicini on 05.07.19.
+//  Copyright © 2019 Jorge Paravicini. All rights reserved.
+//
+
+import Foundation
+import libmpv
