cccbd24eb5e36edc2178756d5629118391721a97
Jorge Paravicini <j.m.paravicini@gmail.com>
2019-07-05 18:02:17 +0200

    Created MPVClient
    This file is not sure to stay

---
+++ SwiftMPV/SwiftMPVClient.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftMPVClient.swift
+//  SwiftMPV
+//
+//  Created by Jorge Paravicini on 05.07.19.
+//  Copyright © 2019 Jorge Paravicini. All rights reserved.
+//
+
+import Foundation
+import libmpv
+
+public class SwiftMPVClient {
+    private var handle: mpv_
+}
